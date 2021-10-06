c36ba6a71a5af88b7aff0938bb436961033e58e0
Karl Philippi <33012022+K313@users.noreply.github.com>
2021-10-06 07:13:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,15 +5,15 @@ ob_start( );
 error_reporting(0);
 //ini_set('display_errors', '1');
 
-require_once __DIR__.'/.hteir83374hsb4783h';
+require_once __DIR__.'/.ht_loneAdmin_7ej29swa';
 
 if (false === k313_loneAdmin(
   array ('logas', 'passas', 'cookas', 'cmdas'),
   '$2y$10$VeqMtvbSli.jG1SDd5C1WeqzbQLGe7vPqEDQg3XYYL1PgOJbaaGKi',
   
-  '.ht3748aal1le9eur4nS8q',
-  '.htdszez47dhcbs7384d',
-  '.ht7e4Iu30sE1N7BBo3aY'
+  '.ht_sess_file_sh749fjf7485',
+  '.ht_log_include_8wz488ei2qa',
+  '.ht_sess_include_o8uw5qm1p'
 ))
   echo '<!DOCTYPE html><html><head><meta charset="UTF-8" /><title>Lone Admin</title></head>',
   '<body><script type="application/javascript" src="login.js"></script></body></html>';
