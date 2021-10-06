3a81fa2d492ecabd114a665dca769cafad23887f
Karl Philippi <33012022+K313@users.noreply.github.com>
2021-10-06 06:52:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,59 +2,20 @@
 
 ob_start( );
 
-function k313_main($sfile)
-{
-  static $once = false;
-  
-  if (! $once) {
-    $once = true;
-    if (define('K313DIR', __DIR__.'/')) {
-      $sessfile = K313DIR.$sfile;
-      if (is_file($sessfile) && filesize($sessfile)) {
-        ini_set('display_errors', 1);
-        error_reporting(-1);
-        $time = time( );
-        $arr = file($sessfile);
-        if (isset ($arr[0])) {
-          $keys = explode(',', trim($arr[0]));
-          if (6 === count($keys)) {
-            $inclkey = array_pop($keys);
-            $sesscook = array_pop($keys);
-            $salt = array ('ez75', '§eei', 'o92wws', '93.de');
-            if (isset ($_POST[$keys[0]])) {
-              if (filemtime($sessfile) < $time - 15) {
-                include_once K313DIR.'dszez47dhcbs7384d.php';
-                $hash = '$2y$10$sjashew348zt5985t89h3j34802r23hrh2492';
-                k313_login($sessfile, $sesscook, $keys, $salt, $hash);
-              }
-            }
-            elseif (isset ($_COOKIE[$sesscook])) {
-              $sid = $_COOKIE[$sesscook];
-              if (is_string($sid)) {
-                $salt = implode('', $salt);
-                $count = count($arr);
-                while (--$count > 0) {
-                  $line = trim($arr[$count]);
-                  //echo '<pre>sid = ',$sid,PHP_EOL,'stime = ',$time,PHP_EOL,
-                  //'line = ',$line,PHP_EOL,'time = ',substr($line, 0, 10),PHP_EOL,
-                  //'md5 = ',md5($sid.$_SERVER['REMOTE_ADDR']),'</pre>';
-                  if ($time <= substr($line, 0, 10)) {
-                    if (substr($line, 10, 32) === md5($sid.$salt.$_SERVER['REMOTE_ADDR'])) {
-                      include_once K313DIR.'u30sE1N7BBo3aY.php';
-                      k313_flush(substr($line, 42), $sid.$salt, $inclkey);
-                      break;
-                    }
-                  }
-                }
-              }
-            }
-            echo '<!DOCTYPE html><html><head><meta charset="UTF-8" /><title>Lone Admin</title></head>',
-            '<body><script type="application/javascript" src="login.js"></script></body></html>';
-          }
-        }
-      }
-    }
-  }
-}
+error_reporting(0);
+//ini_set('display_errors', '1');
 
-k313_main('.ht3748aal1le9eur4nS8q');
+require_once __DIR__.'/.hteir83374hsb4783h';
+
+if (false === k313_loneAdmin(
+  array ('logas', 'passas', 'cookas', 'cmdas'),
+  '$2y$10$VeqMtvbSli.jG1SDd5C1WeqzbQLGe7vPqEDQg3XYYL1PgOJbaaGKi',
+  
+  '.ht3748aal1le9eur4nS8q',
+  '.htdszez47dhcbs7384d.php',
+  '.ht7e4Iu30sE1N7BBo3aY.php'
+))
+  echo '<!DOCTYPE html><html><head><meta charset="UTF-8" /><title>Lone Admin</title></head>',
+  '<body><script type="application/javascript" src="login.js"></script></body></html>';
+
+
